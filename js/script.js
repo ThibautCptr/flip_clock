4800938c61a37f71dd7073d6ff3c146edcce7f2d
ThibautCptr <carpentier.thibaut62@hotmail.fr>
2024-09-19 20:16:51 +0200

    Add necessary files and configurations for Flip Clock

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
-$(document).ready(function() {
-    setInterval(function() {
-        let date = moment();
-        $('#clock').text(date.tz('Europe/Paris').format('H:mm:ss'));
 
-    },10);
-});
+function updateTimeHours(tick) {
+    Tick.helper.interval(function() {
+        let _current_date = moment().tz('Europe/Paris');
+        tick.value = _current_date.format('HH');
+    }, 10);
+}
+
+function updateTimeMinutes(tick) {
+    Tick.helper.interval(function() {
+        let _current_date = moment().tz('Europe/Paris');
+        tick.value = _current_date.format('mm');
+    }, 10);
+}
+
+function updateTimeSeconds(tick) {
+    Tick.helper.interval(function() {
+        let _current_date = moment().tz('Europe/Paris');
+        tick.value = _current_date.format('ss');
+    }, 10);
+}
